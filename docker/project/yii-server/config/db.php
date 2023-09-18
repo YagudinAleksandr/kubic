2cfaa908d9ad8010100e2913e2bc971c1e4b9562
Aleksandr Yagudin <75535457+YagudinAleksandr@users.noreply.github.com>
2023-09-18 02:55:37 -0700

    Залив

---
+++ docker/project/yii-server/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=10.244.0.138:3306;dbname=yiiserver_test',
+    'username' => 'root',
+    'password' => 'admin',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
